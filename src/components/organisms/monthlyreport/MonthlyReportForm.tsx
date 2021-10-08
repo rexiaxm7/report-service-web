2199268d7e345541092737b960f012aa2847f258
B4129 <B4129@oic.co.jp>
2021-10-08 14:25:52 +0900

    👷chore: 月報入力画面の説明を追加

--- src/components/organisms/monthlyreport/MonthlyReportForm.tsx
+++ src/components/organisms/monthlyreport/MonthlyReportForm.tsx
@@ -4,7 +4,17 @@ type Props = {
 
 }
 export const MonthlyReportForm: VFC<Props> = memo((props) => {
+    //日付の状態
+    //月報入力テキストエリアの状態
+    //日付の取得
+    //月報入力テキストエリアのonChange
+    //キャンセルボタンクリック
+    //更新/登録ボタンクリック
     return (
+        //日付インプット
+        //月報入力テキストエリア
+        //キャンセルボタン
+        //更新/登録ボタン
         <div></div>
     )
 })
