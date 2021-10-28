7828e8eff2da5f812dae82648fe7051996fa30bb
B4129 <B4129@oic.jp>
2021-10-28 10:22:13 +0900

    🐞fix: 仮実装

--- src/components/organisms/users/UserDialog.tsx
+++ src/components/organisms/users/UserDialog.tsx
@@ -1,10 +1,16 @@
 import { memo, useEffect, VFC } from "react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import * as yup from "yup";
 import {
+  Box,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
+  FormControl,
   FormControlLabel,
+  FormHelperText,
+  FormLabel,
   Grid,
   IconButton,
   Input,
@@ -15,10 +21,13 @@ import {
   Radio,
   RadioGroup,
   Select,
+  Stack,
+  TextField,
 } from "@mui/material";
 import { OperationButton } from "../../atoms/buttons/OperationButton";
 import { useUserDialog } from "../../../hooks/view/useUserDialog";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 type Props = {};
 
@@ -53,6 +62,47 @@ export const UserDialog: VFC<Props> = memo((props) => {
     setShowPassword,
   } = useUserDialog();
 
+  const schema = yup.object({
+    teamId: yup.number().required("チームを選択してください"),
+    admin: yup.boolean().required("権限を選択してください"),
+    email: yup
+      .string()
+      .required("メールアドレスは必須です")
+      .email("正しいメールアドレスを入力してください"),
+    userName: yup.string().required("ユーザー名を入力してください"),
+    password: yup
+      .string()
+      .required("パスワードは必須です")
+      .min(6, "少ないよ")
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&].*$/,
+        "パスワード弱いよ"
+      ),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<SampleFormInput>({
+    // 追加
+    resolver: yupResolver(schema),
+  });
+
+  // フォームの型
+  interface SampleFormInput {
+    userName: string;
+    teamId: number;
+    admin: boolean;
+    email: string;
+    password: string;
+  }
+
+  const onSubmit: SubmitHandler<SampleFormInput> = (data: any) => {
+    selectedUser ? onClickUpdate(selectedUser?.id) : onClickRegister();
+  };
+
   useEffect(() => {
     if (isUserModalOpen) {
       getTeams();
@@ -72,51 +122,52 @@ export const UserDialog: VFC<Props> = memo((props) => {
         onClose={() => setIsUserModalOpen(false)}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        fullWidth
       >
         <DialogTitle id="alert-dialog-title">
           {selectedUser ? "編集" : "追加"}
         </DialogTitle>
         <DialogContent>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <InputLabel htmlFor={"team"}>チーム</InputLabel>
-              <Select
-                id={"team"}
-                value={`${teamId}`}
-                fullWidth
-                onChange={onChangeTeamId}
-              >
-                <MenuItem value={undefined} disabled>
-                  選択してください
-                </MenuItem>
-                {teams.map((team) => (
-                  <MenuItem value={team.id}>{team.name}</MenuItem>
-                ))}
-              </Select>
-            </Grid>
-            <Grid item xs={12}>
-              <Grid container spacing={3}>
-                {selectedUser && (
-                  <Grid item xs={12}>
-                    <InputLabel htmlFor={"userId"}>ユーザーID</InputLabel>
-                    <Input
-                      id={"userId"}
-                      value={selectedUser?.id}
-                      readOnly
-                      disableUnderline
-                      fullWidth
-                    />
-                  </Grid>
-                )}
-                <Grid item xs={12}>
-                  <InputLabel htmlFor={"email"}>権限</InputLabel>
-                  <RadioGroup
-                    aria-label="authority"
-                    name="radio-group"
-                    value={admin}
-                    onChange={onChangeAdmin}
-                    row
+          <Stack spacing={3} width={"100%"}>
+            <FormControl>
+              <Controller
+                name="teamId"
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    select
+                    label="チーム"
+                    fullWidth
+                    error={Boolean(errors.teamId)}
+                    helperText={errors.teamId?.message}
                   >
+                    {teams.map((team) => (
+                      <MenuItem value={team.id}>{team.name}</MenuItem>
+                    ))}
+                  </TextField>
+                )}
+              />
+            </FormControl>
+
+            {selectedUser && (
+              <FormControl>
+                <InputLabel htmlFor={"userId"}>ユーザーID</InputLabel>
+                <Input
+                  value={selectedUser?.id}
+                  readOnly
+                  disableUnderline
+                  fullWidth
+                />
+              </FormControl>
+            )}
+            <FormControl error={Boolean(errors.admin)}>
+              <FormLabel component="legend">権限</FormLabel>
+              <Controller
+                control={control}
+                name="admin"
+                render={({ field }) => (
+                  <RadioGroup {...field} row>
                     <FormControlLabel
                       value={false}
                       control={<Radio />}
@@ -128,61 +179,58 @@ export const UserDialog: VFC<Props> = memo((props) => {
                       label="チームリーダー"
                     />
                   </RadioGroup>
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel htmlFor={"userName"}>ユーザー名</InputLabel>
-                  <Input
-                    id={"userName"}
-                    value={userName}
-                    fullWidth
-                    onChange={onChangeUserName}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel htmlFor={"email"}>メールアドレス</InputLabel>
-                  <Input
-                    id={"email"}
-                    value={email}
-                    fullWidth
-                    onChange={onChangeEmail}
-                  />
-                </Grid>
-                {!selectedUser ? (
-                  <Grid item xs={12}>
-                    <InputLabel htmlFor="password">パスワード</InputLabel>
-                    <Input
-                      fullWidth
-                      id="password"
-                      type={showPassword ? "text" : "password"}
-                      value={password}
-                      onChange={onChangePassword}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton onClick={onClickShowPassword}>
-                            {showPassword ? <Visibility /> : <VisibilityOff />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                    />
-                  </Grid>
-                ) : (
-                  <></>
                 )}
-              </Grid>
-            </Grid>
-          </Grid>
+              />
+              {errors.admin?.message && (
+                <FormHelperText>{errors.admin?.message}</FormHelperText>
+              )}
+            </FormControl>
+            <TextField
+              label={"ユーザー名"}
+              required
+              error={Boolean(errors.userName)}
+              helperText={errors.userName?.message}
+              {...register("userName")}
+              fullWidth
+            />
+
+            <TextField
+              label={"メールアドレス"}
+              error={Boolean(errors.email)}
+              helperText={errors.email?.message}
+              {...register("email")}
+              fullWidth
+              onChange={onChangeEmail}
+            />
+
+            {!selectedUser ? (
+              <Box>
+                <InputLabel htmlFor="password">パスワード</InputLabel>
+                <Input
+                  fullWidth
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={onChangePassword}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton onClick={onClickShowPassword}>
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </Box>
+            ) : (
+              <></>
+            )}
+          </Stack>
         </DialogContent>
         <DialogActions>
           <OperationButton onClick={() => onClickCancel()} color="inherit">
             キャンセル
           </OperationButton>
-          <OperationButton
-            onClick={() =>
-              selectedUser ? onClickUpdate(selectedUser?.id) : onClickRegister()
-            }
-            color="primary"
-            disabled={!canRegister}
-          >
+          <OperationButton onClick={handleSubmit(onSubmit)} color="primary">
             {selectedUser ? "更新" : "登録"}
           </OperationButton>
         </DialogActions>
